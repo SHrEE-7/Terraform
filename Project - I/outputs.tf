d3c136adb46194139733730244f337cd96994082
Shree <shreekumarf@gmail.com>
2023-01-16 23:20:01 +0530

    output file

--- Project - I/outputs.tf
+++ Project - I/outputs.tf
@@ -1,7 +1,7 @@
 output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-ami-image.id
+  value = data.aws_ami.latest_amazon_ami_image.id
 }
 
 output "ec2-public_ip" {
-  value = aws_instance.myapp-server.public_ip
+  value = aws_instance.myapp_server.public_ip
 }
